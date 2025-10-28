9b50e1451b7fe117c43e9392968022888ba10f47
thilankapathum <thilankapathum@gmail.com>
2025-10-28 12:58:41 +0530

    Back navigation from pulse/cell improved

--- frontend/netrics/src/environments/environment.ts
+++ frontend/netrics/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:8001',
+  // baseUrl: 'http://localhost:8001',
+  baseUrl: 'http://172.19.95.160:8001',
   featureFlag: true
 }
