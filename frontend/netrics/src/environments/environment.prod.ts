b7a3a2fb8e26451ec1481d34ac3490c528231435
thilankapathum <thilankapathum@gmail.com>
2025-11-06 20:28:48 +0530

    Frontend keycloak Working (with docker-compose. No reverse-proxy)

--- frontend/netrics/src/environments/environment.prod.ts
+++ frontend/netrics/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://172.19.95.160:8001',
+  // baseUrl: 'http://172.19.95.160:8001',
+  baseUrl: 'http://netrics.local:8001',
+  // baseUrl: 'http://localhost:8001',
   featureFlag: true
 }
