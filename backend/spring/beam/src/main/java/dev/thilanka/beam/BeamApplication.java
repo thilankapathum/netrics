3004b9712054d3453c933b397a07072f734c4da5
thilankapathum <thilankapathum@gmail.com>
2026-05-11 14:13:43 +0530

    Beam init

---
+++ backend/spring/beam/src/main/java/dev/thilanka/beam/BeamApplication.java
@@ -0,0 +1,13 @@
+package dev.thilanka.beam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeamApplication.class, args);
+	}
+
+}
