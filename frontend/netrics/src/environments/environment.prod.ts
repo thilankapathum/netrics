64fcbbb2a39af6d5bc021c5065f988bf8d5e3c0c
thilankapathum <thilankapathum@gmail.com>
2026-09-02 10:46:17 +0530

    changed environment.prod.ts

--- frontend/netrics/src/environments/environment.prod.ts
+++ frontend/netrics/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://netrics.local:8000',
-  authUrl: 'http://netrics.local:8000/auth',
+  baseUrl: 'http://172.22.41.77:8000',
+  authUrl: 'http://172.22.41.77:8000/auth',
   featureFlag: true
 }
